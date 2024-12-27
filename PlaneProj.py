c393b16d1d48a21c7ba57de0eb9f80b4d5672674
Np3-0 <152457140+Np3-0@users.noreply.github.com>
2024-12-27 13:36:49 -0500

    functionality for both arms

--- PlaneProj.py
+++ PlaneProj.py
@@ -7,6 +7,11 @@ cap = cv2.VideoCapture(0)
 pose = pem.poseDetector()
 #https://en.wikipedia.org/wiki/Law_of_cosines
 
+angles = [0, 0]
+
+# points that are needed for the calculations
+points = [[12, 14, 24], [11, 13, 23]]
+
 def clamp(value, minV, maxV):
     return max(minV, min(value, maxV))
 
@@ -29,20 +34,23 @@ while 1:
     poseLms = pose.getPos(img)
     if not poseLms: continue
     
-    midpoint = getMidPoint(poseLms[12], poseLms[24])
+    for i in range(2):  
+        midpoint = getMidPoint(poseLms[points[i][0]], poseLms[points[i][2]])
     
-    # draws the midpoint and the third line to make it easier to visualize
-    cv2.circle(img, (midpoint[0], midpoint[1]), 5, (0, 0, 255), 3)
-    cv2.line(img, (poseLms[14][1], poseLms[14][2]), (midpoint[0], midpoint[1]), (0, 0, 255), 5)
+        # draws the midpoint and the third line to make it easier to visualize
+        cv2.circle(img, (midpoint[0], midpoint[1]), 5, (0, 0, 255), 3)
+        cv2.line(img, (poseLms[points[i][1]][1], poseLms[points[i][1]][2]), (midpoint[0], midpoint[1]), (0, 0, 255), 5)
     
-    # gets the lengths of each line
-    distances = getDistances([poseLms[12][1], poseLms[12][2]], [poseLms[14][1], poseLms[14][2]], [midpoint[0], midpoint[1]])
+        # gets the lengths of each line
+        distances = getDistances([poseLms[points[i][0]][1], poseLms[points[i][0]][2]], [poseLms[points[i][1]][1], poseLms[points[i][1]][2]], [midpoint[0], midpoint[1]])
     
-    # gets the angle in deg of the armpit (line a and c)
-    angles = getAngles(distances[0], distances[1], distances[2])
-    print(angles)
+        # gets the angle in deg of the armpit (line a and c)
+        angle = getAngles(distances[0], distances[1], distances[2])
+        angles[i] = angle
     
     fps = pose.getFps()
+    cv2.putText(img, f"Left Angle: {angles[1]}", (10, 140), cv2.FONT_HERSHEY_PLAIN, 5, (0, 0, 255), 3)
+    cv2.putText(img, f"Right Angle: {angles[0]}", (10, 210), cv2.FONT_HERSHEY_PLAIN, 5, (0, 0, 255), 3)
     cv2.putText(img, str(int(fps)), (10, 70), cv2.FONT_HERSHEY_PLAIN, 5, (255, 0, 255), 3)
     cv2.imshow("Image", img)
     if cv2.waitKey(1) & 0xFF == ord("q"):

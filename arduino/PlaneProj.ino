5aec478b61c2ad1452aadb0e66ed560e5f8e1836
Np3-0 <152457140+Np3-0@users.noreply.github.com>
2024-12-31 17:05:09 -0500

    added support for 4 servos, will improve plane wings

--- arduino/PlaneProj.ino
+++ arduino/PlaneProj.ino
@@ -1,14 +1,14 @@
 #include <Servo.h>
 
-Servo servos[2];
-const byte servoPins[2] = {3, 4};
+Servo servos[4];
+const byte servoPins[4] = {3, 4, 5, 6};
 int runs = 0;
 
 void setup() {
   Serial.begin(115200);
   Serial.setTimeout(1);
 
-  for(int i = 0; i < 2; i++){
+  for(int i = 0; i < 4; i++){
     servos[i].attach(servoPins[i]);
     servos[i].write(0);
   }
@@ -19,7 +19,13 @@ void loop() {
   while (!Serial.available());
   int x = Serial.readString().toInt();
   // did not feel like dealing with strings so keep a run amount
-  runs % 2 == 0 ? servos[0].write(x) : servos[1].write(x);
+  if (runs % 2 == 0) {
+    servos[0].write(x);
+    servos[1].write(x);
+  } else {
+    servos[2].write(x);
+    servos[3].write(x);
+  }
   delay(100);
   runs++;
 }
